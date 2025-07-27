91fb993701b6654a740ea2058dd81421d3a02d96
ErickDanielMaldonadoAguilar <erickdanielmaldonado2004@gmail.com>
2025-07-26 22:49:13 -0600

    Renombrar dist a docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/tareapokedex/',  // 👈 Esto es lo que hace que funcione en GitHub Pages
   plugins: [
     react(),
     tailwindcss()
